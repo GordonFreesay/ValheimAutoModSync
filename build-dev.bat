102b8844e3f9d2de71b6aebd7f3db8339964b4a6
Gordon Freesay <83382495+GordonFreesay@users.noreply.github.com>
2026-09-23 21:37:59 -0400

    Compile Phase 5 scheduler in dev server

--- build-dev.bat
+++ build-dev.bat
@@ -104,7 +104,7 @@ rem The release builder never defines this symbol, so public binaries do not con
 "%CSC%" @"%REFS%" /target:library /define:AMS_DEV_TESTS /out:"%OUT%\ValheimAutoModSync.Client.dll" "%SOURCE%\ValheimAutoModSync.Client.cs" "%SOURCE%\AutoModSync.PathSafety.cs" "%SOURCE%\AutoModSync.ResumeState.cs"
 if errorlevel 1 exit /b 1
 
-"%CSC%" @"%REFS%" /target:library /define:AMS_DEV_TESTS /out:"%OUT%\ValheimAutoModSync.Server.dll" "%SOURCE%\ValheimAutoModSync.Server.cs" "%SOURCE%\AutoModSync.PathSafety.cs" "%SOURCE%\AutoModSync.ResumeState.cs"
+"%CSC%" @"%REFS%" /target:library /define:AMS_DEV_TESTS /out:"%OUT%\ValheimAutoModSync.Server.dll" "%SOURCE%\ValheimAutoModSync.Server.cs" "%SOURCE%\AutoModSync.PathSafety.cs" "%SOURCE%\AutoModSync.ResumeState.cs" "%SOURCE%\AutoModSync.TransferScheduler.cs"
 if errorlevel 1 exit /b 1
 
 rem Apply uses the same AMS_DEV_TESTS symbol for its deterministic transactional interruption markers.
